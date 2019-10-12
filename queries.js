ac6564bc6b1fb9753279952b1bac9db2763be2c2
umangraval <umangraval2000@gmail.com>
2019-10-13 00:05:05 +0530

    review route

--- queries.js
+++ queries.js
@@ -7,6 +7,9 @@ const pool = new Pool({
   password: '',
   port: 5432,
 })
+var customerid;
+var propertyid;
+
 const getproperty = (request, response) => {
     pool.query('SELECT * FROM property ORDER BY id ASC', (error, results) => {
       if (error) {
@@ -15,23 +18,25 @@ const getproperty = (request, response) => {
       response.status(200).render("properties",{results: results});
     })
   }
+  
   const getpropertyById = (request, response) => {
-    const id = request.params.id
+    const id = request.params.id;
+    propertyid=id;
     pool.query('SELECT * FROM property WHERE id = $1', [id], (error, foundinfo) => {
-      if (error) {
-        response.status(400).json('wrong credentials')
+        pool.query('SELECT comment FROM reviews WHERE proid=$1',[id],(err,reviews)=>{
+        if (error) {
         response.redirect("/property");
-      }
-      response.status(200).render("showproperty",{foundinfo:foundinfo});
+            }
+        response.status(200).render("showproperty",{foundinfo:foundinfo,reviews:reviews});
+        })
     })
   }
-  const getreviews = (request, response) => {
-      const proid = request.params.proid
-    pool.query('SELECT * FROM reviews where proid = $1',[proid], (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)
+  const addreviews = (request, response) => {
+      const comment = request.body.comment;
+     const proid= propertyid;
+     console.log(comment);
+      pool.query('INSERT INTO reviews (comment,proid) VALUES ($1,$2)',[comment,proid], (error, comments) => {
+      response.redirect("/property/"+proid);
     })
   }
   
@@ -46,41 +51,10 @@ const getproperty = (request, response) => {
     //})
   }
 
-//   const handleRegister = (request, response)=>{
-//     const { email,username,password} =request.body;
-//     pool.query('INSERT INTO users (email, username, password) VALUES ($1,$2,$3)', [email,username,password], (error, results) => {
-//       if (error) {
-//         throw error
-//       }
-//       response.status(201).send(`user added`)
-//     })
-//   }
-
-//   const handleSignin = (request,response) => {
-//     const { password, email}  = request.body;
-//     pool.query('SELECT email,password FROM users WHERE password =$1 AND email=$2',[password,email],(error,results)=>{
-//         if(error){
-//             throw error;
-//         }
-//         else{ 
-//             if(results.rowCount === 0){
-//                 response.status(404).send('not found');
-//             }
-//             else if(results.rows[0].password === password && results.rows[0].email === email){
-//             pool.query('INSERT INTO login (email,password) VALUES ($1,$2)',[email,password]);
-//             response.status(404).json({
-//                 email: email,
-//                 username: results.rows[0].username
-//             });
-//             }
-//         }
-//     })
-// }
-const update=(req,res)=>{
-     const place = req.body.place;
+  const update=(req,res)=>{ 
+    const place = req.body.place;
      const name = req.body.name;
      const cost = parseInt(req.body.cost);
-     
      pool.query('INSERT INTO property (place,cost,name) VALUES ($1,$2,$3)', [place,cost,name], (error, results) => {
       if (error) {
         res.render("newproperty");
@@ -97,15 +71,16 @@ const signin = (req,res) => {
 const handlesignin = (req,res) => {
      const email = req.body.email;
      const password = req.body.password;
-     pool.query('SELECT email,password FROM users WHERE email=$1 AND password=$2',[email,password],(err,result)=>{
+     pool.query('SELECT userid,email,password FROM users WHERE email=$1 AND password=$2',[email,password],(err,result)=>{
      if(result.rowCount>0){
         if(result.rows[0].email===email && result.rows[0].password===password){
+            customerid=result.rows[0].userid;
             pool.query('INSERT INTO login (email,password) VALUES ($1,$2)', [email,password], (error, login) => {
                 if (error){
-                  res.render("/siginin");
+                  res.render("siginin");
                 }else{
                     res.redirect("/property");
-                }
+                    }
             })
         }
         else{
@@ -125,22 +100,49 @@ const handleregister = (req,res) => {
      const email = req.body.email;
      const password = req.body.password;
      pool.query('INSERT INTO users (username,email,password) VALUES ($1,$2,$3)',[username,email,password],(err,result)=>{
-        if (error){
-            res.redirect("/register");
+        if (err){
+            res.render("register");
         }else{
+            pool.query('SELECT userid FROM users',(err,uid)=>{
+                customerid=uid.rows[0].userid;
+            })
             res.redirect("/property");
         }
         })
     }
+
+    const rent = (req,res)=> {
+        res.render("rent",{id:propertyid});
+    }
+
+    const rentdetails = (req,res)=>{
+        const person=req.body.person;
+        const duration=req.body.duration;
+        proid = propertyid;
+        id=customerid;
+        pool.query('INSERT INTO renter (cusid,person,duration,propertyid) VALUES ($1,$2,$3,$4)',[id,person,duration,proid],(err,data)=>{
+                res.redirect('/property');
+        })    
+    }
   
+
+    const showtrasaction = (req,res)=> {
+        pool.query('SELECT * FROM transaction WHERE userid=$1',[customerid],(err,data)=>{
+            res.render("showtransactions",{id:propertyid,uid:customerid,data:data});
+        })
+    }
+
     module.exports = {
     getproperty,
     getpropertyById,
     sell,
-    getreviews,
+    addreviews,
     handleregister,
     register,
     update,
     handlesignin,
-    signin
+    signin,
+    rentdetails,
+    rent,
+    showtrasaction
   }
--- server.js
+++ server.js
@@ -14,15 +14,19 @@ app.get('/', (request, response) => {
 app.get('/property',db.getproperty);
 app.get('/property/:id', db.getpropertyById)
 app.get('/sell', db.sell)
-app.get('/:proid/reviews', db.getreviews);
+app.post('/review', db.addreviews);
+//app.get('/review',db.showreviews);
 // app.post('/signin', db.handleSignin);
 // app.post('/register',db.handleRegister);
 // app.post('/property/:proid/rent',db.getrented);
- app.post("/property", db.update);
+app.post("/property", db.update);
 app.get("/signin", db.signin);
 app.post("/signin",db.handlesignin);
 app.get("/register", db.register);
 app.post("/register",db.handleregister);
+app.post("/rentdetails",db.rentdetails);
+app.get("/rent",db.rent)
+app.get("/transaction",db.showtrasaction);
 
 app.listen(3000,function(){
     console.log("server on port 3000");
